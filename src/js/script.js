6f567e7ffa031620ad5393386de80013def82688
friederjanmoerman <hijbrid@gmail.com>
2020-04-14 09:57:27 +0200

    wrote the script for the tab functionality

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+var tabSelectors = document.querySelectorAll('.nav .list__item');
+var tabSelectorsArray = Array.prototype.slice.call(tabSelectors);
+
+var tabs = document.querySelectorAll('.tab');
+var tabsArray = Array.prototype.slice.call(tabs);
+
+console.log(tabSelectors);
+for (var i = 0; i < tabSelectors.length; i++) {
+  tabSelectors[i].addEventListener('click', function() {
+    console.log("clicked");
+    console.log(this);
+    var indexTabSelector = tabSelectorsArray.indexOf(this);
+    console.log(indexTabSelector + " index");
+
+    var tab = tabs.item(indexTabSelector);
+    console.log(tab + " found tab");
+
+    if (this.classList.contains('is-active')) {
+    } else {
+      for (var i = 0; i < tabSelectors.length; i++) {
+        tabSelectors[i].classList.remove('is-active');
+      }
+      this.classList.add('is-active');
+    }
+
+    if (tab.classList.contains('is-active')) {
+    } else {
+      for (var i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove('is-active');
+      }
+      tab.classList.add('is-active');
+    }
+  });
+}
