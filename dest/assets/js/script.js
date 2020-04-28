97a6a75934141621d8ef9144c4aab1dc20fd0314
friederjanmoerman <hijbrid@gmail.com>
2020-04-28 11:26:32 +0200

    closing the popup is possible now and changed the order of the tabs, about page comes first now as requested by client

--- dest/assets/js/script.js
+++ dest/assets/js/script.js
@@ -1,39 +1,61 @@
-// gatherm tab menu buttons
-var tabSelectors = document.querySelectorAll('.nav .list__item');
-var tabSelectorsArray = Array.prototype.slice.call(tabSelectors);
+document.addEventListener('DOMContentLoaded',function(event){
 
-// gather tabs
-var tabs = document.querySelectorAll('.tab');
-var tabsArray = Array.prototype.slice.call(tabs);
+  // gatherm tab menu buttons
+  var tabSelectors = document.querySelectorAll('.nav .list__item');
+  var tabSelectorsArray = Array.prototype.slice.call(tabSelectors);
 
-// tab functionality
-for (var i = 0; i < tabSelectors.length; i++) {
-  // add click onto tab menu buttons
-  tabSelectors[i].addEventListener('click', function() {
-    // get index of clicked tab
-    var indexTabSelector = tabSelectorsArray.indexOf(this);
+  // gather tabs
+  var tabs = document.querySelectorAll('.tab');
+  var tabsArray = Array.prototype.slice.call(tabs);
 
-    // to be activated tab
-    var tab = tabs.item(indexTabSelector);
+  // tab functionality
+  for (var i = 0; i < tabSelectors.length; i++) {
+    // add click onto tab menu buttons
+    tabSelectors[i].addEventListener('click', function() {
+      // get index of clicked tab
+      var indexTabSelector = tabSelectorsArray.indexOf(this);
 
-    // if clicked tab menu button is active, do nothing
-    // else deactivate by removing active class of active tab menu button and active clicked tab menu button by adding class
-    if (this.classList.contains('is-active')) {
-    } else {
-      for (var i = 0; i < tabSelectors.length; i++) {
-        tabSelectors[i].classList.remove('is-active');
+      // to be activated tab
+      var tab = tabs.item(indexTabSelector);
+
+      // if clicked tab menu button is active, do nothing
+      // else deactivate by removing active class of active tab menu button and active clicked tab menu button by adding class
+      if (this.classList.contains('is-active')) {
+      } else {
+        for (var i = 0; i < tabSelectors.length; i++) {
+          tabSelectors[i].classList.remove('is-active');
+        }
+        this.classList.add('is-active');
       }
-      this.classList.add('is-active');
+
+      // if clicked tab is active, do nothing
+      // else deactivate by removing active class of active tab and active clicked tab by adding class
+      if (tab.classList.contains('is-active-tab')) {
+      } else {
+        for (var i = 0; i < tabs.length; i++) {
+          tabs[i].classList.remove('is-active-tab');
+        }
+        tab.classList.add('is-active-tab');
+      }
+    });
+  }
+
+
+  /*
+   * close gallery pop up
+   */
+
+  (function() {
+    'use strict';
+
+    var btnClosePopUp = document.querySelector('#close-pop-up');
+    var popUp = document.querySelector('.gallery-pop-up');
+
+    function closePopUp() {
+      console.log("click");
+      popUp.classList.remove('is-active');
     }
 
-    // if clicked tab is active, do nothing
-    // else deactivate by removing active class of active tab and active clicked tab by adding class
-    if (tab.classList.contains('is-active-tab')) {
-    } else {
-      for (var i = 0; i < tabs.length; i++) {
-        tabs[i].classList.remove('is-active-tab');
-      }
-      tab.classList.add('is-active-tab');
-    }
-  });
-}
+    btnClosePopUp.addEventListener('click', closePopUp);
+  })();
+});
